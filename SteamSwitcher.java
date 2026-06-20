d068c22957b2203617a534cb4257ef23a0457266
pichaccu <pichaccu@gmail.com>
2026-06-20 19:47:15 +0200

    Bump version to 1.0.4

--- SteamSwitcher.java
+++ SteamSwitcher.java
@@ -28,7 +28,7 @@ import java.util.regex.*;
  */
 public class SteamSwitcher extends JFrame {
 
-    static final String VERSION = "1.0.3";
+    static final String VERSION = "1.0.4";
     static final String REPO    = "pichaccu/steam-switcher";
 
     // ── Colors ────────────────────────────────────────────────────────────────
